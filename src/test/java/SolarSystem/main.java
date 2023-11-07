86dfcebf1b7e40dcea90d10c12b14cbf8176040a
MayraMejia <mayramejia21018@gmail.com>
2023-11-07 12:48:02 -0500

    the commit has the basic structure of the solar system

---
+++ src/test/java/SolarSystem/main.java
@@ -0,0 +1,13 @@
+package SolarSystem;
+
+public class main {
+
+    public static void main(String[] args) {
+        solarSystemBasics solars= new solarSystemBasics();
+        solars.Sun="1";
+        solars.Moon="2";
+        solars.Planets="2";
+        solars.Stars="10";
+
+    }
+}
